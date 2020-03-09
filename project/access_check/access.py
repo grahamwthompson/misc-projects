d5c11538eb4644f1f40b6aa275805b995a8aec22
gray <gray@2b.lan>
2020-03-08 17:10:18 -0700

    working script

--- project/access_check/access.py
+++ project/access_check/access.py
@@ -1,27 +1,25 @@
 #!/usr/bin/env python
-
 import click, os, json, time
 
 def formatTimestamp(stamp):
     return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(stamp))
 
-print('Check a folder for modifications/access or checkpoint a folder for later comparison')
-
-# command line arguments should be put here somewhere 
 @click.group()
 def access():
+    '''Command line tool to check file access metadata'''
     pass
 
 @click.command()
 @click.argument('folder')
 @click.argument('checkpoint_file')
 def checkpoint(folder, checkpoint_file):
-    click.echo('checkpoint function')
-    
+    '''Create a checkpoint of access metadata'''
+    click.echo('Creating checkpoint for {} at {}'.format(folder, checkpoint_file))
+
     with open(checkpoint_file, 'w') as cf:
         checkpoints = {}
-        
-        for root, dirs, files in os.walk(".", topdown=False):
+
+        for root, dirs, files in os.walk(folder, topdown=False):
             for name in files:
                 config_line = {}
                 stat = os.stat(os.path.join(root,name))
@@ -30,57 +28,46 @@ def checkpoint(folder, checkpoint_file):
                 config_line['access_time'] = stat.st_atime
                 config_line['modification_time'] = stat.st_mtime
                 config_line['creation_time'] = stat.st_ctime
-                
+
                 checkpoints[name] = config_line
 
-            for name in dirs:
-                config_line = {}
-                stat = os.stat(os.path.join(root,name))
-                config_line['name'] = name
-                config_line['size'] = stat.st_size
-                config_line['access_time'] = stat.st_atime
-                config_line['modification_time'] = stat.st_mtime
-                config_line['creation_time'] = stat.st_ctime
-                
-                checkpoints[name] = config_line
-        
         cf.write(json.dumps(checkpoints))
 
 @click.command()
 @click.argument('folder')
 @click.argument('checkpoint_file')
 def check(folder, checkpoint_file):
-    click.echo('check access against checkpoint')
-    
+    '''Check for access since checkpoint'''
+    click.echo('Checking access against checkpoint: \n')
+
     checkpoints = {}
     with open(checkpoint_file, 'r') as cf:
         checkpoints = json.load(cf)
 
-    print(checkpoints)
-    for root, dirs, files in os.walk(".", topdown=False):
+    for root, dirs, files in os.walk(folder, topdown=False):
         for name in files:
             if name in checkpoints:
                 # check for file differences
                 info = checkpoints[name]
                 stat = os.stat(os.path.join(root,name))
                 changed = False
-                access_string = name + ': '
+                access_string = root + '/' + name + ': '
 
                 if info['access_time'] != stat.st_atime:
                     changed = True
-                    access_string += 'accessed - {} '.format(formatTimestamp(stat.st_atime))
+                    access_string += 'accessed-{} '.format(formatTimestamp(stat.st_atime))
                 if info['modification_time'] != stat.st_mtime:
                     changed = True
-                    access_string += 'modified - {} '.format(formatTimestamp(stat.st_mtime))
+                    access_string += 'modified-{} '.format(formatTimestamp(stat.st_mtime))
                 if info['creation_time'] != stat.st_mtime:
                     changed = True
-                    access_string += 'created - {} '.format(formatTimestamp(stat.st_ctime))
-                
+                    access_string += 'created-{} '.format(formatTimestamp(stat.st_ctime))
+
                 if changed:
                     click.echo(access_string)
 
             else:
-                click.echo('{} - added'.format(name))
+                click.echo('{}/{}: added'.format(root, name))
 
 
 access.add_command(checkpoint)
@@ -88,19 +75,3 @@ access.add_command(check)
 
 if __name__ == '__main__':
     access()
-
-
-# if checkpointing go here 
-
-# # create a new checkpoint file to store our checkpoint data
-# # recursively itterate through all files in the given directory and output their name, size, 
-# # created, modified, and accessed dates to the checkpoint file (maybe a hash too)
-
-
-# if checking for changes go here 
-
-# # load our specified checkpoint file 
-
-# # recursively itterate through all files and check for any differences between their checkpoint 
-# # created, modified and accessed dates and their current ones
-# # also list files that were deleted or added
